ff639aa58aab3aaa7513b09ab17f6610efd3bb4c
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-05-17 19:58:48 +0800

    Update portal page

--- content/caddy_init_en.sh
+++ content/caddy_init_en.sh
@@ -7,12 +7,12 @@ else
     DRIVENAME=${RCLONE_DRIVE_NAME}
 fi
 
-if [ "${RCLONE_AUTO_MODE}" = "move" ]; then
-    sed -i "s|MODE_STATUS|<b>[move] Move files to Rclone remote ${DRIVENAME}:${RCLONE_UPLOAD_DIR} on torrent completion</b>|g" /workdir/homer/assets/config.yml
-elif [ "${RCLONE_AUTO_MODE}" = "custom" ]; then
+if [ "${RCLONE_AUTO_MODE}" = "custom" ]; then
     sed -i "s|MODE_STATUS|<b>[custom]</b>|g" /workdir/homer/assets/config.yml
 elif [ ! -f "/mnt/data/config/rclone.conf" ]; then
     sed -i "s|MODE_STATUS|<b>Fail to find Rclone config file</b>|g" /workdir/homer/assets/config.yml
+elif [ "${RCLONE_AUTO_MODE}" = "move" ]; then
+    sed -i "s|MODE_STATUS|<b>[move] Move files to Rclone remote ${DRIVENAME}:${RCLONE_UPLOAD_DIR} on torrent completion</b>|g" /workdir/homer/assets/config.yml
 elif [ "${RCLONE_AUTO_MODE}" = "copy" ]; then
     sed -i "s|MODE_STATUS|<b>[copy] Copy files to Rclone remote ${DRIVENAME}:${RCLONE_UPLOAD_DIR} on torrent completion</b>|g" /workdir/homer/assets/config.yml
 fi
